8dea48efa0edacd4efad42108e5e1bf14068212d
Erykxon Bacasmo <114896787+erykxon@users.noreply.github.com>
2023-06-20 19:03:44 +0800

    fix some minimal syntax here

--- profile.php
+++ profile.php
@@ -33,6 +33,7 @@ if(!isset($_SESSION['id']) || (trim($_SESSION['id']) == '')){
 <body>
     <h1>Welcome user <?php echo $rows['full_name']?></h1><br><br>
     <a href="index.php">Back</a> &nbsp; &nbsp;
-    <a href="edit.php?id= <?php echo $rows['stud_id']?>">Edit Record</a>
+    <a href="edit.php?id= <?php echo $rows['stud_id']?>">Edit Record</a><br><br>
+
 </body>
 </html>
